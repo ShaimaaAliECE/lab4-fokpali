025049eb419da96c79790f1426338a6bf2fca15f
fokpali <71132881+fokpali@users.noreply.github.com>
2021-11-27 15:28:50 -0500

    commiting all files

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+let questionsList = require('./questions.json');
+
+const app = express();
+
+// serve static contents
+app.use(express.static('static'));
+
+// dynamic handling
+
+app.get('/questionsInJson', (req,res) => {
+    res.json(questionsList);
+})
+
+app.get('/answers/:ans/question/:que', (req,res) => {
+    let answer=req.params.ans;
+    let question=req.params.que;
+    let array='';
+    for(a of questionsList)
+    {
+         array+=a.answerIndex;
+    }
+    if(answer==array[question])
+    { 
+        res.send("Correct!");
+    }
+    else  res.send("Incorrect!");
+})
+app.get('/score/:leng/selections/:anschose', (req,res) => {
+    let length=req.params.leng;
+    let answerChosen=req.params.anschose;
+    let score=0;
+    let x=0;
+    for(a of questionsList)
+    {
+       if (answerChosen[x]==a.answerIndex)
+       {score++;}
+       x++;
+    }
+    res.send('<h3>Your score is: <h3>'+score +"/"+length+"<br><i>Please refresh to start again</i>");
+})
+
+app.listen(802);
